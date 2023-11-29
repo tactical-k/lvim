6d82e099e80563d2b44a66cdf648c2c397f25ec0
tactical-k <58684184+tactical-k@users.noreply.github.com>
2023-11-30 02:03:13 +0900

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,56 @@
+lvim.plugins = {
+  {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v2.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      window = {
+        width = 30,
+      },
+      buffers = {
+        follow_current_file = true,
+      },
+      filesystem = {
+        follow_current_file = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules"
+          },
+          never_show = {
+            ".DS_Store",
+            "thumbs.db"
+          },
+        },
+      },
+    })
+  end
+  },
+  {
+  'wfxr/minimap.vim',
+  build = "cargo install --locked code-minimap",
+  -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+  config = function ()
+    vim.cmd ("let g:minimap_width = 10")
+    vim.cmd ("let g:minimap_auto_start = 1")
+    vim.cmd ("let g:minimap_auto_start_win_enter = 1")
+  end,
+  },
+  {
+  "nacro90/numb.nvim",
+    event = "BufRead",
+    config = function()
+    require("numb").setup {
+     show_numbers = true, -- Enable 'number' for the window while peeking
+     show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+    }
+    end,
+  },
+}
